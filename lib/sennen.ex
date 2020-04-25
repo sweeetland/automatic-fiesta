0a639b1f19f7568a1e11a7e4b78f45aff339a7d6
sweeetland <jamesadamsweetland@gmail.com>
2020-04-25 10:08:09 +0100

    generate random locations

--- lib/sennen.ex
+++ lib/sennen.ex
@@ -1,2 +1,16 @@
 defmodule Sennen do
+  @number_of_locations 9
+
+  def start do
+    generate_n_locations(@number_of_locations)
+    |> IO.inspect()
+  end
+
+  defp generate_n_locations(n) do
+    Enum.map(1..n, &generate_random_location/1)
+  end
+
+  defp generate_random_location(_) do
+    {Enum.random(-180..180), Enum.random(-180..180)}
+  end
 end
