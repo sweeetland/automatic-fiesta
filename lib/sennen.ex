74423a6d81c6632018d7a0b557f268639a0797d1
sweeetland <jamesadamsweetland@gmail.com>
2020-04-25 10:10:46 +0100

    fetch stats

--- lib/sennen.ex
+++ lib/sennen.ex
@@ -1,8 +1,11 @@
 defmodule Sennen do
+  @max_concurrency 3
   @number_of_locations 9
+  @ss_base_url "https://api.sunrise-sunset.org/json?formatted=0"
 
   def start do
     generate_n_locations(@number_of_locations)
+    |> fetch_stats()
     |> IO.inspect()
   end
 
@@ -12,5 +15,23 @@ defmodule Sennen do
 
   defp generate_random_location(_) do
     {Enum.random(-180..180), Enum.random(-180..180)}
+  end
+
+  defp fetch_stats(locations) do
+    locations
+    |> Task.async_stream(&get_stats_for_location/1, max_concurrency: @max_concurrency)
+    |> Enum.into([], fn {:ok, res} -> res end)
+  end
+
+  defp get_stats_for_location({lat, lng}) do
+    case HTTPoison.get("#{@ss_base_url}&lat=#{lat}&lng=#{lng}") do
+      {:ok, %{status_code: 200, body: body}} ->
+        {:ok, parsed_body} = Jason.decode(body)
+        parsed_body["results"]
+
+      {_, _} ->
+        IO.puts("could not fetch stats for location [#{lat}, #{lng}]")
+        {:error}
+    end
   end
 end
